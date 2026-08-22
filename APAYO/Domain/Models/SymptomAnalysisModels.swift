2ff1fbc4ceb49bec30466f5dd87d56bcf34d023a
dojmj <joahae@ewhain.net>
2026-08-23 02:57:25 +0900

    fix(ai): encode nullable request fields correctly

--- APAYO/Domain/Models/SymptomAnalysisModels.swift
+++ APAYO/Domain/Models/SymptomAnalysisModels.swift
@@ -16,11 +16,49 @@ struct SymptomAnalysisRequest: Encodable, Equatable {
         case allowedCardIDs = "allowed_card_ids"
         case allowedQuestionIDs = "allowed_question_ids"
     }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encode(schemaVersion, forKey: .schemaVersion)
+        try container.encode(input, forKey: .input)
+        try container.encode(allowedCardIDs, forKey: .allowedCardIDs)
+        try container.encode(allowedQuestionIDs, forKey: .allowedQuestionIDs)
+
+        if let workContext {
+            try container.encode(workContext, forKey: .workContext)
+        } else {
+            try container.encodeNil(forKey: .workContext)
+        }
+
+        if let weatherContext {
+            try container.encode(weatherContext, forKey: .weatherContext)
+        } else {
+            try container.encodeNil(forKey: .weatherContext)
+        }
+    }
 }
 
 struct SymptomInput: Encodable, Equatable {
     let text: String
     let languageHint: String?
+
+    enum CodingKeys: String, CodingKey {
+        case text
+        case languageHint = "language_hint"
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encode(text, forKey: .text)
+
+        if let languageHint {
+            try container.encode(languageHint, forKey: .languageHint)
+        } else {
+            try container.encodeNil(forKey: .languageHint)
+        }
+    }
 }
 
 struct WorkContext: Codable, Equatable {
@@ -34,6 +72,20 @@ struct WorkContext: Codable, Equatable {
         case workType = "work_type"
         case pesticideExposure = "pesticide_exposure"
         case injuryOrFall = "injury_or_fall"
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encode(workedToday, forKey: .workedToday)
+        try container.encode(pesticideExposure, forKey: .pesticideExposure)
+        try container.encode(injuryOrFall, forKey: .injuryOrFall)
+
+        if let workType {
+            try container.encode(workType, forKey: .workType)
+        } else {
+            try container.encodeNil(forKey: .workType)
+        }
     }
 }
 
